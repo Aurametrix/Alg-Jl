9e357fef0754a762a50197c2efaf669da7f930a7
Irene <irene@aurametrix.com>
2014-02-15 10:04:16 -0800

    more built-in sort examples

--- SORT/sort.jl
+++ SORT/sort.jl
@@ -5,3 +5,14 @@ sort([2,3,1], rev=true)
 p = sortperm(a) #compute a permutation to put array into sorted order
 sort(a, by=abs) #sort ac. to arbitrary transformation of values
 sort(v, alg=InsertionSort) #chose insertion sort
+
+# build-in sort
+A = [("UK", "London"), ("US", "New York"), ("US", "Birmingham"), ("UK", "Birmingham")];
+sort(A, by=x -> x[2])
+###
+4-element Array{(ASCIIString,ASCIIString),1}:
+ ("US","Birmingham")
+ ("UK","Birmingham")
+ ("UK","London")
+ ("US","New York") 
+###
