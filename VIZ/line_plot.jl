a18699d3bb71551c41ee29da27c14d292d97c6b2
Irene <irene@aurametrix.com>
2014-12-13 19:18:27 -0800

    line & scatter plot

--- VIZ/line_plot.jl
+++ VIZ/line_plot.jl
@@ -6,13 +6,21 @@ Plotly.signin("Julia-Demo-Account", "hvkrsbg3uj")
 trace1 = [
   "x" => [1, 2, 3, 4], 
   "y" => [10, 15, 13, 17], 
+  "mode" => "markers", 
   "type" => "scatter"
 ]
 trace2 = [
-  "x" => [1, 2, 3, 4], 
-  "y" => [16, 5, 11, 9], 
+  "x" => [2, 3, 4, 5], 
+  "y" => [16, 5, 11, 9],
+  "mode" => "lines", 
   "type" => "scatter"
 ]
-data = [trace1, trace2]
-response = Plotly.plot(data, ["filename" => "basic-line", "fileopt" => "overwrite"])
+trace3 = [
+  "x" => [1, 2, 3, 4], 
+  "y" => [12, 9, 15, 12], 
+  "mode" => "lines+markers", 
+  "type" => "scatter"
+]
+data = [trace1, trace2, trace3]
+response = Plotly.plot(data, ["filename" => "line-scatter", "fileopt" => "overwrite"])
 plot_url = response["url"]
